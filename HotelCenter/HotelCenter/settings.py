f91c53ab81eedb737aacfbaed03d9b8a5854c3ce
Nima <56191102+nima-kam@users.noreply.github.com>
2022-03-08 21:30:37 +0330

    rebased on user model

--- HotelCenter/HotelCenter/settings.py
+++ HotelCenter/HotelCenter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    "user",
+    'Hotel',
     'main',
 
     "user",
@@ -90,7 +93,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'testdb.sqlite3',
     }
 }
-
+if 'test' in sys.argv:
+    DATABASES['default'] = DATABASES['TEST']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
