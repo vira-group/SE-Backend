1b4335029d7c23ef80f4f905b696bdcb5b92240d
Nima <56191102+nima-kam@users.noreply.github.com>
2022-03-16 20:11:32 +0330

    user tests created

--- HotelCenter/main/tests.py
+++ HotelCenter/main/tests.py
@@ -1,3 +1,54 @@
+import http
 from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from .models import Hotel
+
+
+class UserTestCase(APITestCase):
+    test_urls = {
+        "hotel-list": "/hotel/hotels/",
+        "hotel-obj": "/hotel/hotels/{}/",
+    }
+
+    def setUp(self) -> None:
+        """
+            RUNS BEFORE EACH TEST
+        """
+        self.user1_data = {
+            "email": "",
+            'password': "",
+            "repassword": "",
+        }
+
+    def set_credential(self, token):
+        """
+            set token for authorization
+        """
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+    def unset_credential(self):
+        """
+            unset existing headers
+        """
+        self.client.credentials()
+
+
+    def test_register_success(self):
+        pass
+
+    def test_register_fields_miss(self):
+        pass
+
+    def test_login_success(self):
+        pass
+
+    def test_login_fail(self):
+        pass
+
+    def test_activation(self):
+        pass
