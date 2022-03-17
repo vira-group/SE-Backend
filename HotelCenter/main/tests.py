3e817f5a897fb408e6c9bd41c735dc7c584c59c9
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2022-03-17 14:19:30 +0330

    Login tests added

--- HotelCenter/main/tests.py
+++ HotelCenter/main/tests.py
@@ -71,3 +71,29 @@ class UserRegisterAPITests(TestCase):
         self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
+class UserLoginApiTest(APITestCase):
+    def setUp(self) -> None:
+        data = {"email":"hello@gmail.com","password":"strongpass","re_password":"strongpass"}
+        self.client.post("/auth/users/", data)
+        user = get_user_model().objects.get(email = "hello@gmail.com")
+        user.is_active = True
+        user.save()
+        data1 = {"email":"hello1@gmail.com","password":"strongpass1","re_password":"strongpass1"}
+        self.client.post("/auth/users/", data)
+        user = get_user_model().objects.get(email = "hello@gmail.com")
+        user.save()
+
+    def test_login_normal(self):
+        data = {"email":"hello@gmail.com", "password":"strongpass"}
+        response = self.client.post("/auth/token/login/", data)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_login_without_emailVerification(self):
+        data = {"email":"hello1@gmail.com", "password":"strongpass1"}
+        response = self.client.post("/auth/token/login/", data)
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login_wrong_password(self):
+        data = {"email":"hello1@gmail.com", "password":"str"}
+        response = self.client.post("/auth/token/login/", data)
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
