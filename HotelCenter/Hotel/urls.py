61fbe44db8d34e5ecd66bdf48daf9e7de27b7036
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2022-04-30 20:07:56 +0430

    api to get reservation list of a roomspace added

--- HotelCenter/Hotel/urls.py
+++ HotelCenter/Hotel/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework import routers
 from .api.hotel import HotelViewSet, FacilityViewSet, HotelImgViewSet, BestHotelViewSet, MyHotelsViewSet
 from .api.room import RoomList, roomFacilityViewSet, ImageList, RoomSpaceViewSet
+from .api.reserve import RoomspaceReserveList
 
 router = routers.DefaultRouter()
 router.register('hotels', HotelViewSet, basename='user-hotel')
@@ -36,6 +37,7 @@ room_router.register('spaces', RoomSpaceViewSet, basename='room-space')
 urlpatterns = [
     path('room/<int:hotel_id>/', RoomList.as_view()),
     path('room/<int:room_id>/images/', ImageList.as_view()),
+    path('reserve/roomspace/<int:roomspace_id>/', RoomspaceReserveList.as_view()),
     path('', include(router.urls)),
     path('<int:hid>/', include(hotel_router.urls)),
 
