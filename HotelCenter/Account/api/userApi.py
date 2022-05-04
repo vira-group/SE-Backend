28b767a472024a3cdbdf28ee5080733e35421db8
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2022-05-04 19:13:01 +0430

    profile and edit profiel tests added

--- HotelCenter/Account/api/userApi.py
+++ HotelCenter/Account/api/userApi.py
@@ -3,7 +3,8 @@ from ..serializers.user_serializers import PublicUserSerializer, MyProfileSerial
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import viewsets
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
 
@@ -16,6 +17,7 @@ class publicUserDetail(viewsets.ViewSet, generics.RetrieveAPIView):
     serializer_class = PublicUserSerializer
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def myProfileDetail(request):
     """
     Retrieve, update or delete a code snippet.
--- HotelCenter/HotelCenter/settings.py
+++ HotelCenter/HotelCenter/settings.py
@@ -173,7 +173,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "hotelcenter.noreply@gmail.com"
-EMAIL_HOST_PASSWORD = "Vira1400SE"
+EMAIL_HOST_PASSWORD = "Vira1400SE1"
 # DEFAULT_FROM_EMAIL = 'HotelCenter <no_reply@domain.com>'
 
 DJOSER = {
