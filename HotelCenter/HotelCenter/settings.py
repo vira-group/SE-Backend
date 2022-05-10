b459d77b69b7365ffa3557a78b23674510fe4e39
Nima <56191102+nima-kam@users.noreply.github.com>
2022-05-10 16:20:54 +0430

    settings changed

--- HotelCenter/HotelCenter/settings.py
+++ HotelCenter/HotelCenter/settings.py
@@ -93,6 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
+
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'Hotel_center',
