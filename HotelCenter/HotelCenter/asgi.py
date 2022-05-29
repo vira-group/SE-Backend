787d614c6556546bb8f41016818405add7da327a
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2022-05-30 01:01:20 +0430

    asgi problems

--- HotelCenter/HotelCenter/asgi.py
+++ HotelCenter/HotelCenter/asgi.py
@@ -6,8 +6,6 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
-import django
-django.setup()
 # mysite/asgi.py
 import os
 import channels
@@ -18,6 +16,9 @@ from django.core.asgi import get_asgi_application
 import Chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HotelCenter.settings")
+
+import django
+django.setup()
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
