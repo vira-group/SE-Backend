7b1b95210feb656259cafe931f919c1b9ccdc9d1
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2022-05-30 00:31:07 +0430

    models.py

--- HotelCenter/Chat/models.py
+++ HotelCenter/Chat/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-User = get_user_model()
+from Account.models import User
 class Message(models.Model):
     author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
     content = models.TextField()
