40f55c64ebf5183c409572b622e8a5551b2a98a8
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2022-07-02 17:45:51 +0430

    username for chat changed

--- HotelCenter/Chat/views.py
+++ HotelCenter/Chat/views.py
@@ -12,5 +12,5 @@ def index(request):
 def room(request, room_name):
     return render(request, 'chat/room.html', {
        'room_name': mark_safe(json.dumps(room_name)),
-        'username': mark_safe(json.dumps(request.user.username)),
+        'id': mark_safe(json.dumps(request.user.id)),
     })
