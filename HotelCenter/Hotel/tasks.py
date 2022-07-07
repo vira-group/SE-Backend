4f3eaede4640cbbca72c81e335ae1ca495b00949
Nima <56191102+nima-kam@users.noreply.github.com>
2022-07-07 17:53:19 +0430

    all hotel serializer is favorite fixed

--- HotelCenter/Hotel/tasks.py
+++ HotelCenter/Hotel/tasks.py
@@ -1,8 +1,12 @@
+import datetime
+
 from django.conf import settings
 from django.core.mail import EmailMessage, get_connection
 from celery import shared_task
 from celery.utils.log import get_task_logger
 from django.core.mail import send_mail, BadHeaderError
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 from django.conf import settings
 
 from templated_email import send_templated_mail
@@ -13,11 +17,29 @@ send_templated_mail(
         from_email='from@example.com',
         recipient_list=['to@example.com'],
         context={  # context to be loaded in the template
-            'username': request.user.username,
-            'full_name': request.user.get_full_name(),
-            'signup_date': request.user.date_joined
+            'username': "hoi",
+            'full_name': "guededag",
+            'start_date': "2020-02-03"
         },
     )
+    
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+
+subject = 'Subject'
+html_message = render_to_string('reserve/reserve_reminder.html', {
+            'username': "hadi",
+            'hotel': "parsian",
+            'start_date': "2020-02-03",
+            'room': "room_type",
+            "size": "size 45",
+            "hotel_url":"localhost"
+        })
+plain_message = strip_tags(html_message)
+from_email = settings.EMAIL_HOST_USER
+to = 'dakey43331@shbiso.com'
+
+send_mail(subject, plain_message, from_email, [to], html_message=html_message)
 """
 
 logger = get_task_logger(__name__)
@@ -31,26 +53,44 @@ def pre_reserve(Reserve, **kwargs):
     user = Reserve.user
     username = user.username
     email = user.email
+    room = Reserve.room.type
+    size = Reserve.room.size
+    start_date = Reserve.start_day
+    hotel = Reserve.room.hotel.name
 
-    send_templated_mail(
-        template_name=template_name,
-        from_email=settings.EMAIL_HOST_USER,
-        recipient_list=[email],
-        context={
-            "start_date": Reserve.start_day,
+    subject = 'Reminder for your Reservation'
 
-            "end_date": Reserve.end_day,
-            "hotel": Reserve.room.hotel,
+    html_message = render_to_string(template_name, context={
+        'username': username,
+        'hotel': hotel,
+        'start_date': start_date,
+        'room': room,
+        "size": size,
+        "hotel_url": "localhost"
+    })
+    plain_message = strip_tags(html_message)
+    from_email = settings.EMAIL_HOST_USER
+    to = email
 
-            'room': Reserve.room.type,
-            # 'hotel_url':,
-            'username': username,
-        }
-    )
+    send_mail(subject, plain_message, from_email, [to], html_message=html_message)
+
 
 @shared_task
-def set_reserve_tasks(Reserve,**kwargs):
+def set_reserve_tasks(Reserve, **kwargs):
+    """
+    set before and after reserve tasks
+    """
 
+    now = datetime.datetime.now() + datetime.timedelta(minutes=20)
+    # if now < Reserve.start_day:
+    rem_date = max(Reserve.start_day - datetime.timedelta(days=2), now)
+
+    feedback_date = max(Reserve.end_day + datetime.timedelta(days=1), now)
+
+    pre_reserve.apply_async((Reserve, kwargs), eta=rem_date)
+    after_reserve.apply_async((Reserve, kwargs), eta=feedback_date)
+
+    logger.info("schedule review email")
 
 @shared_task(name="after_reserve")
 def after_reserve(Reserve, **kwargs):
@@ -61,18 +101,19 @@ def after_reserve(Reserve, **kwargs):
     username = user.username
     email = user.email
 
-    send_templated_mail(
-        template_name=template_name,
-        from_email=settings.EMAIL_HOST_USER,
-        recipient_list=[email],
-        context={
-            "start_date": Reserve.start_day,
+    subject = 'Reminder for your Reservation'
+    html_message = render_to_string(template_name, context={
 
-            "end_date": Reserve.end_day,
-            "hotel": Reserve.room.hotel,
+        "hotel_url": "localhost"
+    })
+    plain_message = strip_tags(html_message)
+    from_email = settings.EMAIL_HOST_USER
+    to = email
 
-            'room': Reserve.room.type,
-            # 'hotel_url':,
-            'username': username,
-        }
-    )
+    send_mail(subject, plain_message, from_email, [to], html_message=html_message)
+
+
+@shared_task(name="add_for_test")
+def add(a, b):
+    logger.info("Sent review email")
+    return a, b
