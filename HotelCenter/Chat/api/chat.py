a1f7d5396afc0b1471089a5ddeb2c5221da09f44
Hediyeh-Eshaqi <hediyeh.eshaqi@gmail.com>
2022-07-08 00:25:28 +0430

    user chatlist api edited

--- HotelCenter/Chat/api/chat.py
+++ HotelCenter/Chat/api/chat.py
@@ -14,26 +14,38 @@ class MessageAPI(APIView):
         pass
 
 
+# class UserChatList(APIView):
+
+#     permission_classes = [IsAuthenticated]
+#     def get(self, request, format=None):
+#         messages = Message.objects.all()
+#         chatlist = []
+#         for message in messages:
+#             if(self.isvalid(message.chat, str(request.user.id))):
+#                 if(not message in chatlist):
+#                     chatlist.append(message)
+#         serializer = MessageSerializer(chatlist, many=True)
+#         return Response(serializer.data)
+
+#     def isvalid(self, chatname, id):
+#         try:
+#             if (chatname.split("t")[1]==id or chatname.split("t")[0] == id ):
+#                 return True
+#             return False
+#         except:
+#             return False
+
 class UserChatList(APIView):
 
     permission_classes = [IsAuthenticated]
     def get(self, request, format=None):
-        messages = Message.objects.all()
-        chatlist = []
-        for message in messages:
-            if(self.isvalid(message.chat, str(request.user.id))):
-                if(not message in chatlist):
-                    chatlist.append(message)
-        serializer = MessageSerializer(chatlist, many=True)
+        user = request.user
+        chatlist = HotelChat.objects.filter(user = user).all()
+        serializer = HotelChatSerializer(chatlist, many=True) 
         return Response(serializer.data)
 
-    def isvalid(self, chatname, id):
-        try:
-            if (chatname.split("t")[1]==id or chatname.split("t")[0] == id ):
-                return True
-            return False
-        except:
-            return False
+
+
 
 class HotelChatAPI(APIView):
     def get(self, request, hotel_id, format=None):
