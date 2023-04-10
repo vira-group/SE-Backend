aec0f60398775def4aeb22028d39d835bbbc2fe7
amin geramimehr <m.a.geramimehr.0000@gmail.com>
2023-04-10 21:58:29 +0330

    develop : add new permission  customer
     this permission can allow to  customer users only that use Apis that
    this permission implement on taht APIs

--- HotelCenter/HotelCenter/permissions.py
+++ HotelCenter/HotelCenter/permissions.py
@@ -11,3 +11,14 @@ class IsManager(BasePermission):
          if request.user.role == 'M':
         
              return True
+         
+class IsCustomer(BasePermission):
+    
+    '''
+    Allows access only Customer  
+        '''
+    def has_permission(self, request, view):
+       
+         if request.user.role == 'C':
+        
+             return True
