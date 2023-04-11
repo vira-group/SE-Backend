454d6b4375ca1d23883f61dc49e7f40a1b9391d3
amin geramimehr <m.a.geramimehr.0000@gmail.com>
2023-04-11 18:42:00 +0330

    develop : craete ticket model
    sender field , text field , created_date

--- HotelCenter/ticket/models.py
+++ HotelCenter/ticket/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
-# Create your models here.
+from  Account.models import User
+# Create your model
+class  TicketForm(models.Model):
+    sender=models.ForeignKey(User,on_delete=models.CASCADE)
+    text =models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    
