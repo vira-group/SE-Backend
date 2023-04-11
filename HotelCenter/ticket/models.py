a793514f78194a205e776af61e4844ba57ea6f60
amin geramimehr <m.a.geramimehr.0000@gmail.com>
2023-04-11 18:55:43 +0330

    develop : update new ticket models  and add new models in name of RequestForm
    add new fields  in names of  status fileds ,  name , ticketform
     # Please enter the commit message for your changes. Lines starting

--- HotelCenter/ticket/models.py
+++ HotelCenter/ticket/models.py
@@ -2,7 +2,20 @@ from django.db import models
 from  Account.models import User
 # Create your model
 class  TicketForm(models.Model):
+    
+    class TaskStatus(models.TextChoices):
+            PENDING = 'P', 'Pending'
+            WAITING = 'W', 'Waiting'
+            ASSIGNED = 'A', 'Assigned'
+            DONE = 'D', 'Done'
+            
     sender=models.ForeignKey(User,on_delete=models.CASCADE)
     text =models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
-    
+    status= models.CharField(max_length=1,default=TaskStatus.PENDING,choices=TaskStatus.choices)
+
+
+class RequestForm(models.Model):
+     name=models.CharField(max_length=155)
+     ticketform  = models.ForeignKey(TicketForm, on_delete=models.CASCADE)
+     
