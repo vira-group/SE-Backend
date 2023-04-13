042ecfb948d2f95eb83d627c31a4aeb6f3758fbd
amin geramimehr <m.a.geramimehr.0000@gmail.com>
2023-04-13 19:55:19 +0330

    develop : add new api
    this api is used for response admin

--- HotelCenter/ticket/views.py
+++ HotelCenter/ticket/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination 
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateAPIView
 from rest_framework import status
 from .serializers import RequestFormSerializer,TicketFormSerializer,AdminTicketSerializer
 from .models import RequestForm,TicketForm
@@ -14,11 +14,17 @@ from .models import RequestForm,TicketForm
 
 
 
-class TypeRequestsList(ListCreateAPIView):
+class TypeRequestsList(ListCreateAPIView): #get list type request and post it 
     serializer_class=RequestFormSerializer
     queryset=RequestForm.objects.all();
 
 
-class MyTicketList(ListCreateAPIView):
+class MyTicketList(ListCreateAPIView):   #get list ticket and post it 
     serializer_clas=TicketFormSerializer
     queryset= TicketForm.objects.all()
+    
+    
+class ResponseAdminList(RetrieveUpdateAPIView): ### get one ticket and update it 
+    queryset=TicketForm.objects.all()
+    serializer_class=AdminTicketSerializer
+    
