0076210e81afb6120e62da249613afa3b4806e79
amin geramimehr <m.a.geramimehr.0000@gmail.com>
2023-04-13 18:57:04 +0330

    develop : add imports to view.py
    this imports are for  ticket Apies

--- HotelCenter/ticket/views.py
+++ HotelCenter/ticket/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_list_or_404
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination 
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
+from .serializers import RequestFormSerializer,ShowTicketSerializer,TicketFormSerializer,AdminTicketSerializer
 # Create your views here.
