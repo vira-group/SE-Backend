9306597ab8ad763ebd83d9345f26489a51c4fe3e
amin geramimehr <m.a.geramimehr.0000@gmail.com>
2023-04-15 13:42:03 +0330

    fix & develop :  fix and devleop comment model
    add two models on names of tag and reply
    fix comment model for reply

--- HotelCenter/comment/models.py
+++ HotelCenter/comment/models.py
@@ -11,9 +11,27 @@ class Comment(models.Model):
     )
     writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name="comments")
     hotel = models.ForeignKey(Hotel, related_name='comments', on_delete=models.CASCADE)
-    text = models.CharField(max_length=1024, null=False, blank=False)
+    text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
+    reply=models.OneToOneField("Reply", related_name="comment",on_delete=models.SET_NULL,null=True)
+    tag=models.ManyToManyField("Tag", related_name="comment") 
 
     class Meta:
-        ordering = ['-modified_at']
+        ordering = ['-created_at']
+
+
+class Reply(models.Model):
+       text_reply=models.TextField()
+       created_at = models.DateTimeField(auto_now_add=True)
+       
+       class Meta:
+         ordering = ['-created_at']
+        
+
+
+class Tag(models.Model):
+    name=models.CharField(max_length=40)
+        
+    def __str__(self) -> str:
+        return  self.name
+    
