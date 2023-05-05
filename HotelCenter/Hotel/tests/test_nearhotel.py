f0b2547f57ae5e1c5c7bf08d30951166783f311d
mohammad amin geramimehr <59396870+MohammadAmin-2001@users.noreply.github.com>
2023-05-05 14:17:11 +0330

    test : add new test for neatr hotel

--- HotelCenter/Hotel/tests/test_nearhotel.py
+++ HotelCenter/Hotel/tests/test_nearhotel.py
@@ -107,13 +107,13 @@ class NearHotel(APITestCase):
             self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
 
     def test_is_not_None(self):
-        # request1=self.client.get(reverse("nearhotel"),self.myloc1).json()
-        # request2=self.client.get(reverse("nearhotel"),self.myloc2).json()
-        # request3=self.client.get(reverse("nearhotel"),self.myloc3).json()
+        request1=self.client.get(reverse("nearhotel"),self.myloc1).json()
+        request2=self.client.get(reverse("nearhotel"),self.myloc2).json()
+        request3=self.client.get(reverse("nearhotel"),self.myloc3).json()
         
-        # self.assertNotEqual(request1,None)
-        # self.assertNotEqual(request2,None)
-        # self.assertNotEqual(request3,None)
+        self.assertNotEqual(request1,None)
+        self.assertNotEqual(request2,None)
+        self.assertNotEqual(request3,None)
         pass 
     def test_result(self):
         
@@ -121,7 +121,36 @@ class NearHotel(APITestCase):
         request2=self.client.get(reverse("nearhotel"),self.myloc2).json()
         request3=self.client.get(reverse("nearhotel"),self.myloc3).json()
         
-        self.assertEqual({2,3},{i["id"] for i in request1})
-        self.assertEqual({2,3},{i["id"] for i in request2})
-        self.assertEqual({3,1},{i["id"] for i in request3})
+        self.assertEqual([2,3],[i["id"] for i in request1])
+        self.assertEqual([2,3],[i["id"] for i in request2])
+        self.assertEqual([1],[i["id"] for i in request3])
+    
+    def test_length_is_ok_but_result_not_ok(self):
+        request1=self.client.get(reverse("nearhotel"),self.myloc1).json()
+        request2=self.client.get(reverse("nearhotel"),self.myloc2).json()
+        request3=self.client.get(reverse("nearhotel"),self.myloc3).json()
+        r1=[i["id"] for i in request1]
+        r2=[i["id"] for i in request2]
+        r3=[i["id"] for i in request3]
+        self.assertEqual(len(r1),2)
+        self.assertTrue(1 not in r1)
+        self.assertEqual(len(r2),2)
+        self.assertTrue(1 not in r2)
+        self.assertEqual(len(r3),1)
+        self.assertTrue(2 not in r3)
+    
+    def test_withot_parametr(self):
+        request1=self.client.get(reverse("nearhotel")).json()
+        r1=[i["id"] for i in request1]
+        self.assertEqual([2,3],r1)
+        self.assertNotEqual(r1,None)
+        self.assertNotEqual(r1,[])
+
+    def test_status_code(self):
+        request1=self.client.get(reverse("nearhotel"),self.myloc1)
+        request2=self.client.get(reverse("nearhotel"),self.myloc2)
+        request3=self.client.get(reverse("nearhotel"),self.myloc3)
         
+        self.assertEqual(request1.status_code,200)
+        self.assertEqual(request2.status_code,200)
+        self.assertEqual(request3.status_code,200)
